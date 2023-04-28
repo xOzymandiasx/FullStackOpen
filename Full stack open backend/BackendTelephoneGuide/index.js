9ad1298a70794e3ed1e4937f403a37a775bfe763
Sergio Ottati <sergioottati@hotmail.com>
2023-04-28 20:40:57 -0300

    Finalizado ejercicios 3.1-3.6

---
+++ Full stack open backend/BackendTelephoneGuide/index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+let persons = [
+  {
+    id: 1,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 2,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 3,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+  {
+    id: 4,
+    name: "Sergio",
+    number: "21345",
+  },
+  {
+    id: 5,
+    name: "Ikki",
+    number: "21345",
+  },
+];
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * max);
+};
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((item) => item.id === id);
+  person ? response.json(person) : response.status(404).end();
+});
+
+app.get("/info", (request, response) => {
+  const date = new Date();
+  response.send(`
+  <p>Phonebook has info for ${persons.length} people</p>
+  <p>${date}</p>
+  `);
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  const existingName = persons.find((item) => item.name === body.name);
+  if (!body.name || !body.number)
+    return response.status(400).json({ error: "error missing" });
+
+  if (existingName)
+    return response.status(400).json({ error: `${body.name} alerady exist` });
+
+  const person = {
+    id: getRandomInt(1000),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons.concat(person);
+  response.json(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((item) => item.id !== id);
+  response.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT);
