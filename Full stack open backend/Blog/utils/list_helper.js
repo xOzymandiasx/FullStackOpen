9a7d20b906bad89ad4fcc97e52048f700e1d63a4
Sergio Ottati <sergioottati@hotmail.com>
2023-05-16 20:40:39 -0300

    Haciendo pruebas en Jest

--- Full stack open backend/Blog/utils/list_helper.js
+++ Full stack open backend/Blog/utils/list_helper.js
@@ -1,16 +1,29 @@
 const dummy = (blogs) => {
-  return 1
+  return 1;
 };
 
 const totalLikes = (blogs) => {
   const reducer = (sum, item) => sum + item;
 
   const arrayLikes = [];
-  blogs.forEach(item => arrayLikes.push(item.likes));
+  blogs.forEach((item) => arrayLikes.push(item.likes));
 
-  return blogs.length === 1 
-   ? blogs[0].likes
-   : arrayLikes.reduce(reducer, 0);
-}
+  return blogs.length === 0
+    ? 0
+    : blogs.length === 1
+    ? blogs[0].likes
+    : arrayLikes.reduce(reducer, 0);
+};
 
-module.exports = {dummy, totalLikes};
+const favoriteBlog = (blogs) => {
+  const blogWithMoreLikes = blogs.reduce((max, shot) => {
+    return shot.likes >= max.likes ? shot : max;
+  });
+  delete blogWithMoreLikes.__v;
+  delete blogWithMoreLikes.url;
+  delete blogWithMoreLikes._id;
+
+  return blogWithMoreLikes;
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog };
