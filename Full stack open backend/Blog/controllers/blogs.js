cb30256dbdba014c0b69cc391b03ecdce015b2b9
Sergio Ottati <sergioottati@hotmail.com>
2023-05-22 21:57:39 -0300

    Empezando con los ejercicios en Supertest

--- Full stack open backend/Blog/controllers/blogs.js
+++ Full stack open backend/Blog/controllers/blogs.js
@@ -1,19 +1,26 @@
 const blogRouters = require("express").Router();
 const Blog = require("../models/blog");
 
-blogRouters.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    console.log(blogs);
-    response.json(blogs);
-  });
+blogRouters.get("/", async (request, response) => {
+  try {
+    const getBlogs = await Blog.find({});
+    response.status(200).json(getBlogs);
+  }catch(error) {
+    console.log(error)
+  }
+  // Blog.find({}).then((blogs) => {
+  //   console.log(blogs);
+  //   response.json(blogs);
+  // });
 });
 
-blogRouters.post("/", (request, response) => {
+blogRouters.post("/", async (request, response) => {
   const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
+  // blog.save().then((result) => {
+  //   response.status(201).json(result);
+  // });
 });
 
 module.exports = blogRouters;
--- Full stack open backend/Blog/tests/blog.test.js
+++ Full stack open backend/Blog/tests/blog.test.js
@@ -1,8 +1,62 @@
+const mongoose = require("mongoose");
+const superTest = require("supertest");
+const Blog = require("../models/blog");
+const app = require("../app")
 const dummy = require("../utils/list_helper").dummy;
 const totalLikes = require("../utils/list_helper").totalLikes;
 const favoriteBlog = require("../utils/list_helper").favoriteBlog;
 const mostBlogs = require("../utils/list_helper").mostBlogs;
 const mostLikes = require("../utils/list_helper").mostLikes;
+
+const api = superTest(app);
+
+const blogs = [
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+  },
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+  },
+  {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+  },
+  {
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+  },
+  {
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+  },
+  {
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+  }  
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  for(let blog of blogs) {
+    const newBlog = new Blog(blog);
+    await newBlog.save();
+  };
+});
 
 test("Dummy return 1", () => {
   const blogs = [];
@@ -24,56 +78,6 @@ describe("total likes", () => {
       likes: 5,
       __v: 0
     }
-  ];
-  const blogs = [
-    {
-      _id: "5a422a851b54a676234d17f7",
-      title: "React patterns",
-      author: "Michael Chan",
-      url: "https://reactpatterns.com/",
-      likes: 7,
-      __v: 0
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0
-    },
-    {
-      _id: "5a422b3a1b54a676234d17f9",
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-      likes: 12,
-      __v: 0
-    },
-    {
-      _id: "5a422b891b54a676234d17fa",
-      title: "First class tests",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-      likes: 10,
-      __v: 0
-    },
-    {
-      _id: "5a422ba71b54a676234d17fb",
-      title: "TDD harms architecture",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-      likes: 0,
-      __v: 0
-    },
-    {
-      _id: "5a422bc61b54a676234d17fc",
-      title: "Type wars",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-      likes: 2,
-      __v: 0
-    }  
   ];
 
   test("When list dosnt have blogs", () => {
@@ -145,7 +149,23 @@ describe("Cuantity of likes in blogs", () => {
     expect(favoriteBlog(blogs)).toEqual(blogs[2]);
   });
 
-  test("Hola", ()=> {
-    mostLikes(blogs);
+  test("Author with most blogs", ()=> {
+    expect(mostBlogs(blogs)).toEqual({ author: 'Robert C. Martin', blogs: 3 });
   });
+
+  test("Blog with more likes", ()=> {
+    expect(mostLikes(blogs)).toEqual({author: 'Edsger W. Dijkstra', likes: 17});
+  });
+});
+
+test("Blogs are returned as Json", () => {
+  api
+  .get("/api/blogs")
+  .expect(200)
+  .expect("Conten-Type", /application\/json/);
+
+}, 10000);
+
+afterAll(() => {
+  mongoose.connection.close();
 });
