000926276f3256941896a61ff4fa9a233c2d7682
Sergio Ottati <sergioottati@hotmail.com>
2023-08-30 11:21:37 -0300

    Más ReactRouter.

---
+++ FullStackOpen-ReactRouter/src/reactRouterPractice/components/ReactRouterAppMain.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import Users from './Users';
+import Notes from './Notes';
+
+const ReactRouterAppMain = () => {
+  //*Forma "casera" de aplicar ReactRouter
+  // const [page, setPage] = useState("home");
+  // const toPage = (page) => (e) => {
+  //   e.preventDefault()
+  //   setPage(page);
+  // };
+  // const content = () => {
+  //   if (page === "home") {
+  //     return <Home />;
+  //   } else if (page === "users") {
+  //     return <Users />;
+  //   } else if (page === "notes") {
+  //     return <Notes />;
+  //   }
+  // };
+
+
+  return (
+    <Router >
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/notes">Notes</Link>
+        <Link to="/users">Users</Link>
+      </div>
+
+      <Routes>
+        <Route path='/notes' element={<Notes />}/>
+        <Route path='/users' element={<Users />}/>
+        <Route path='/' element={<Home />}/>
+      </Routes>
+    </Router>
+
+  );
+};
+
+export default ReactRouterAppMain;
